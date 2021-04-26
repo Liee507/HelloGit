f8ffb047b22b2d14dd082cfa57c7b51583565656
Liee507 <2226772848@qq.com>
2021-04-26 22:47:36 +0800

    克隆更新2

--- HelloGit/src/main/java/com/xiao/git/HelloGit.java
+++ HelloGit/src/main/java/com/xiao/git/HelloGit.java
@@ -14,5 +14,6 @@ public class HelloGit {
         System.out.println("分支更新");
         System.out.println("在线更新");
         System.out.println("克隆添加");
+        System.out.println("克隆添加1");
     };
 }
