b2563ba9e8893406041b576b8d55bbdba9201eac
Liee507 <2226772848@qq.com>
2021-04-26 21:42:34 +0800

    提交添加

---
+++ HelloGit/src/main/java/com/xiao/git/HelloGit.java
@@ -0,0 +1,12 @@
+package com.xiao.git;
+
+/**
+ * @author Liee
+ * @create 2021-04-26 21:34
+ */
+public class HelloGit {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
